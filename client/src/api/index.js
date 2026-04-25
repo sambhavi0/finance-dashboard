8f7d72b1cdfdf4ac2436205728593159191d854a
Sambhavi <sambhavi067@gmail.com>
2026-04-25 23:54:47 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const NODE = 'https://finance-dashboard-server-6g1l.onrender.com/api';
-const PYTHON = 'https://finance-dashboard-analytics.onrender.com';
+const PYTHON = 'finance-dashboard-analytics.onrender.com/analytics/monthly-trend';
 
 // Expense APIs
 export const addExpense = (data) =>
